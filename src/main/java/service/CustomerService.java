cdf5659f6f2625394ceac32350793ae12dc6a0ab
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-16 21:35:55 -0600

    Implementing the Singleton pattern - in process

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -3,18 +3,30 @@ package service;
 import model.Customer;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 //All of the service classes use static references to create singleton objects.
 public class CustomerService {
-    public void addCustomer(String email, String name, String lastName){
 
+    Collection<Customer> customers = new HashSet<Customer>();
+
+    //Define the Singleton
+    public static CustomerService customerServiceSingleton = null;
+
+    CustomerService(){
+
+    }
+
+    public void addCustomer(String email, String name, String lastName){
+        Customer newCustomer = new Customer(email, name, lastName);
+        customers.add(newCustomer);
     }
 
     public Customer getCustomer(String customerEmail){
-
+        return null;
     }
 
     public Collection<Customer> getAllCustomers(){
-
+        return customers;
     }
 }
