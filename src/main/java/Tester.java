0fb6f3dba2c204e1561a0a6df82411ae9ade0353
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-20 11:59:40 -0600

    Testing the singletons working along with the storages

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,8 +1,17 @@
-import model.Customer;
+import model.*;
+import service.CustomerService;
 
 public class Tester {
     public static void main(String[] args) {
-        Customer customer = new Customer("first", "second", "jdomain.com");
-        System.out.println(customer);
+        // Create new Free Room
+        Room myFreeRoom = new FreeRoom("1", RoomType.SINGLE);
+        System.out.println(myFreeRoom.getRoomPrice());
+
+        // Add new customer
+
+        // Add the new customer to the CustomerService
+        CustomerService.getInstance().addCustomer("Luis","Perez","luis@gmail.com");
+        System.out.println(CustomerService.getInstance().getAllCustomers());
+        // Print all customers
     }
 }
