37de20415cc9fb4b048982bde92d996854425165
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-20 12:14:30 -0600

    This are Singleton classes

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -9,28 +9,28 @@ import java.util.Objects;
 //All of the service classes use static references to create singleton objects.
 public class CustomerService {
 
+
+    /*
+    private and empty constructor. No arguments needed
+     */
+    private CustomerService(){}
+
     /*
     Define the storage customers by using:
     Set: the entries won't duplicate
     Hash: every entry has a key to access the content
      */
-
-    Collection<Customer> customers = new HashSet<Customer>();
+    Collection<Customer> customersList = new HashSet<Customer>();
 
     //Define the Singleton object
     public static CustomerService customerServiceSingleton = null;
 
-    /*
-    private and empty constructor. No arguments needed
-     */
-    private CustomerService(){
-    }
 
     /*
-    getInstance() returns the existent object previously created
-     */
+     getInstance() returns the existent object previously created
+    */
     public static CustomerService getInstance(){
-        if(Objects.isNull(customerServiceSingleton){
+        if(Objects.isNull(customerServiceSingleton)){
             customerServiceSingleton = new CustomerService();
         }
         return customerServiceSingleton;
@@ -39,23 +39,25 @@ public class CustomerService {
     /*
     Method to add a customer once the Singleton exists
      */
-    public void addCustomer(String email, String name, String lastName){
-        Customer newCustomer = new Customer(email, name, lastName);
-        customers.add(newCustomer);
+    public void addCustomer(String name, String lastName, String email){
+        Customer newCustomer = new Customer(name, lastName, email);
+        customersList.add(newCustomer);
     }
 
     /*
     Returns the customer found using the email
      */
     public Customer getCustomer(String customerEmail){
-        for(Customer customer:customers){
-            if(customerEmail.equals(customer.getEmail()){
+        for(Customer customer:customersList){
+            if(customerEmail.equals(customer.getEmail())){
                 return customer;
             }
         }
+        return null;
     }
 
     public Collection<Customer> getAllCustomers(){
-        return customers;
+        return customersList;
     }
+
 }
--- src/main/java/service/ReservationService.java
+++ src/main/java/service/ReservationService.java
@@ -6,6 +6,8 @@ import model.Reservation;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
 
 //All of the service classes use static references to create singleton objects.
 
@@ -24,27 +26,80 @@ private
 default
  */
 public class ReservationService {
-    public void addRoom(IRoom room){
-
+    /*
+    The constructor is private so the class cannot be instantiated
+    */
+    private ReservationService() {
     }
 
-    public IRoom getARoom(String roomId){
+    /*
+    Static reference to the class
+     */
+    public static ReservationService reservationServiceSingleton = null;
+
+    /*
+    Storage for reservations, Rooms, and Free Rooms;
+     */
+    Collection<Reservation> reservations = new HashSet<>();
+    Collection<IRoom> roomsList = new HashSet<>();
+    Collection<IRoom> freeRoomsList = new HashSet<>();
+
+    /*
+    Method that returns the instance when it exists
+     */
+    public static ReservationService getInstance() {
+        if (Objects.isNull(reservationServiceSingleton)) {
+            reservationServiceSingleton = new ReservationService();
+        }
+        return reservationServiceSingleton;
+    }
+
+    /*
+    Add a room to the storage
+     */
+    public void addRoom(IRoom room) {
+        roomsList.add(room);
+    }
+
+    /*
+    Find a room when you provide the room number
+     */
+    public IRoom getARoom(String roomNumber) {
+
+        for (IRoom room : roomsList) {
+            if (roomNumber.equals(room.getRoomNumber())) {
+                return room;
+            }
+        }
         return null;
     }
 
-    public Reservation reserveARoom(Customer customer, IRoom room, Date checkinDate, Date checkoutDate){
+    public Collection<IRoom> getAllRooms() {
+        return roomsList;
+    }
+
+    /*
+        Create new Reservation
+     */
+    public Reservation reserveARoom(Customer customer, IRoom room, Date checkinDate, Date checkoutDate) {
+        return new Reservation(customer, room, checkinDate, checkoutDate);
+    }
+
+    /*
+    Find rooms using a range of dates
+     */
+    public Collection<IRoom> findRooms(Date checkinDate, Date checkoutDate) {
+        for (IRoom room : roomsList) {
+            //if(room.getChe)
+        }
         return null;
     }
 
-    public Collection<IRoom> findRooms(Date checkinDate, Date checkoutDate){
+    public Collection<Reservation> getCustomersReservation(Customer customer) {
         return null;
     }
 
-    public Collection<Reservation> getCustomersReservation(Customer customer){
-        return null;
-    }
-
-    public void printAllReservation(){
+    public void printAllReservation() {
 
     }
 }
