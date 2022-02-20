58f9c9c901ba78d6e901f27046b8c26c18086403
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-20 11:39:55 -0600

    Shows menu to the user

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class MainMenu {
 
     public static void main(String[] args) {
-        System.out.println("1. Find and Reserve a Room");
-        System.out.println("2. See my Reservations");
-        System.out.println("3. Create an Account");
-        System.out.println("4. Admin");
-        System.out.println("5. Exit");
+
+int userOption=5;
+        do {
+            System.out.println("1. Find and Reserve a Room");
+            System.out.println("2. See my Reservations");
+            System.out.println("3. Create an Account");
+            System.out.println("4. Admin");
+            System.out.println("5. Exit");
+            System.out.println("\nSelect an Option from the Menu:");
+            Scanner scanner = new Scanner(System.in);
+            userOption=scanner.nextInt();
+        }while(userOption <= 4);
     }
 }
