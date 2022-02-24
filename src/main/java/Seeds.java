7dc93165cb87efb1806bedc5ad364ca0851aeda3
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-24 06:59:44 -0600

    A Reservation is added at start

--- src/main/java/Seeds.java
+++ src/main/java/Seeds.java
@@ -1,24 +1,46 @@
-import model.FreeRoom;
-import model.IRoom;
-import model.Room;
-import model.RoomType;
+import model.*;
 import service.CustomerService;
 import service.ReservationService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Seeds {
     public static void main(String[] args) {
-        CustomerService.getInstance().addCustomer("Hugo","Perez","hp@gmail.com");
-        CustomerService.getInstance().addCustomer("Paco","Lopez","pl@gmail.com");
-        CustomerService.getInstance().addCustomer("Luis","Martinez","lm@gmail.com");
 
-        IRoom Room001 = new  Room("001",100.00, RoomType.SINGLE);
-        IRoom Room002 = new  Room("002",100.00, RoomType.DOUBLE);
-        IRoom Room003 = new FreeRoom("003", RoomType.DOUBLE);
+    }
+
+    public void seedData() throws ParseException {
+        CustomerService.getInstance().addCustomer("Hugo", "Perez", "hp@gmail.com");
+        CustomerService.getInstance().addCustomer("Paco", "Lopez", "pl@gmail.com");
+        CustomerService.getInstance().addCustomer("Luis", "Martinez", "lm@gmail.com");
+
+        IRoom Room001 = new Room("001", 100.00, RoomType.SINGLE);
+        IRoom Room002 = new Room("002", 100.00, RoomType.DOUBLE);
+        IRoom Room003 = new Room("003", 100.00, RoomType.DOUBLE);
+        IRoom Room004 = new Room("004", 100.00, RoomType.DOUBLE);
+        IRoom Room005 = new Room("005", 100.00, RoomType.DOUBLE);
+        IRoom Room006 = new Room("006", 100.00, RoomType.DOUBLE);
+
+
         ReservationService.getInstance().addRoom(Room001);
         ReservationService.getInstance().addRoom(Room002);
         ReservationService.getInstance().addRoom(Room003);
+        ReservationService.getInstance().addRoom(Room004);
+        ReservationService.getInstance().addRoom(Room005);
+        ReservationService.getInstance().addRoom(Room006);
 
-        System.out.println(CustomerService.getInstance().getAllCustomers());
-        System.out.println(ReservationService.getInstance().getAllRooms());
+        Date date1 = new SimpleDateFormat("mm-dd-yyyy").parse("10-26-2022");
+        Date date2 = new SimpleDateFormat("mm-dd-yyyy").parse("10-27-2022");
+        Customer customer1 = CustomerService.getInstance().getCustomer("hp@gmail.com");
+        IRoom Room1 = ReservationService.getInstance().getARoom("001");
+        Reservation reservation1 = ReservationService.getInstance().reserveARoom(customer1, Room1, date1, date2);
+        ReservationService.getInstance().addReservation(reservation1);
+        System.out.println(ReservationService.getInstance().getAllReservations());
+
+//        System.out.println(CustomerService.getInstance().getAllCustomers());
+//        System.out.println(ReservationService.getInstance().getAllRooms());
     }
+
 }
